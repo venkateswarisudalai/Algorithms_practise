47a97309b0c44d30cb218bf1d43aa81b6e4b3a8e
venkateswarisudalai <37179283+venkateswarisudalai@users.noreply.github.com>
2022-01-24 20:11:07 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,40 +1,37 @@
 package com.company;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
     private static int[] twosumBruteforce(int target, int nums[]) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; i++) {
-                if (nums[i] + nums[j] == target)
-                    return new int[]{i, j};
+
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i <= nums.length; i++) {
+            //2 7 11 15   9 --> 0 1
+            //{2, 3, 7, 9, 11}; 20
+            int complement = target - nums[i];
+            //7
+            if (map.containsKey(complement)) {
+//                System.out.println(map.get(complement));
+//                System.out.println(i);
+                return new int[]{map.get(complement), i};
+            } else {
+                map.put(nums[i], i);
+                System.out.println(nums[i]);
+                System.out.println(i);
+
             }
         }
+        return new int[]{};
 
-    return new int[]{};
+    }
+        public static void main (String[]args){
+            int[] nums= new int[]{2, 3, 7, 9, 11};
+            int target = 20;
+            twosumBruteforce(target, nums);
 
-}
-    public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
-    int n = input.nextInt();
-    int[] nums = new int[n];
-    for(int i = 0;i<n;i++)
-    {
-        nums[i] = input.nextInt();
-    }
-int target = input.nextInt();
-    input.close();
-    int[] indices = twosumBruteforce(target,nums);
+        }
 
-
-    if (indices.length == 2)
-    {
-        System.out.println(indices[0] + "" + indices[1]);
-    }
-    else
-    {
-        System.out.println("No solution found");
-    }
-    }
 }
